357ad47839ee09a047451594398a74afb3deca27
Nisha Devi <nishadevi0210@gmail.com>
2021-05-07 12:14:49 +0200

    initial commits

---
+++ src/Components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div>
+            body here
+        </div>
+    )
+}
+
+
+export default Body
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            header here
+        </div>
+    )
+}
+
+export default Header
